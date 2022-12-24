cb3a6c3c56e5d609177b8e2485044cbef5f45145
sarthak_nawaria <sarthak.nawaria.41@gmail.com>
2022-12-24 20:49:11 +0530

    section 4 completed

--- components/footer.js
+++ components/footer.js
@@ -2,8 +2,10 @@ import React from 'react'
 
 function footer() {
   return (
-    <div className="text-3xl">footer</div>
-  )
+    <footer>
+      
+    </footer>
+    )
 }
 
 export default footer;
--- pages/index.js
+++ pages/index.js
@@ -7,14 +7,16 @@ import Format from "../layout/format";
 import Section1 from "../components/section1.js";
 import Section2 from "../components/section2.js";
 import Section3 from "../components/section3.js";
+import Section4 from "../components/section4.js";
 
 export default function Home() {
   return (
     <div>
-      <Format>s
+      <Format>
         <Section1></Section1>
         <Section2></Section2>
         <Section3></Section3>
+        <Section4></Section4>
       </Format>
     </div>
   );
