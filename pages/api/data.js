a7240f5a8bb808984607cae6922ec2d43be2d903
sarthak_nawaria <sarthak.nawaria.41@gmail.com>
2023-01-29 09:09:47 +0530

    changing trending

--- pages/api/data.js
+++ pages/api/data.js
@@ -2,7 +2,8 @@ export default {
   Trending: [
     {
       id: 1,
-      title: "Vote For The Environment: Quantitative characteristics of shareholder resolution votes on environmental issues",
+      title:
+        "Vote For The Environment: Quantitative shareholder resolution votes on environmental issues",
       subtitle:
         "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.",
       category: "Business, Travel",
@@ -20,14 +21,13 @@ export default {
     },
     {
       id: 2,
-      title: "Your most unhappy customers are your greatest source of learning",
+      title: "23 Best islands to visit in 2023: unique island escapes",
       subtitle:
-        "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.",
+        "Who doesn’t love the island life? Whether you want to lounge poolside with a coconut in hand, lose yourself in a book, or head inland for a rugged adventure, our oceans are occupied by a near-endless list of best islands to visit.",
       category: "Business, Travel",
       img: "/images/img2.jpg",
       description: `
-            Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind 
-            text by the name of Lorem Ipsum decided to leave for the far World of Grammar.
+      So, join me on a journey to some of the most beautiful islands in the world, all offering that little something extra. While you won’t see the likes of Bora Bora and Tahiti in French Polynesia here, you’ll find plenty of islands ripe for adventures, wildlife, culture, history and food.
             `,
       published: "Jun 5,  2022",
       author: {
@@ -38,14 +38,13 @@ export default {
     },
     {
       id: 3,
-      title: "Your most unhappy customers are your greatest source of learning",
+      title: "Big Tech Is Really Bad at Firing People",
       subtitle:
         "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.",
       category: "Business, Travel",
       img: "/images/img3.jpg",
       description: `
-            Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind 
-            text by the name of Lorem Ipsum decided to leave for the far World of Grammar.
+      Tech companies have laid off tens of thousands of workers over the past few months in an industry-wide downsizing that executives have blamed on over-hiring during the pandemic.
             `,
       published: "Jun 7,  2022",
       author: {
